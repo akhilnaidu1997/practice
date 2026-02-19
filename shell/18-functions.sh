28d70096dbaa66613247543edbcdbfc46a7158e9
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 20:51:12 +0530

    shell

---
+++ shell/18-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+    echo "ERROR:: please proceed with sudo previlages"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation is failed"
+    else
+        echo "$2 installation is success"
+    fi
+}
+
+dnf install mysql -y 
+VALIDATE $? mysql
