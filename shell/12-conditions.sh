d7c4642a76b1370ea38e72633f08d5d3b469300b
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 18:41:47 +0530

    shell

--- shell/12-conditions.sh
+++ shell/12-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "given number is equal to 10"
 else
     echo "given number is greater than 10"
 fi
