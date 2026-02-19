90549e09d8181a482206103cb21028cb0b29e3f5
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 18:40:52 +0530

    shell

--- shell/12-conditions.sh
+++ shell/12-conditions.sh
@@ -2,8 +2,8 @@
 
 NUMBER=$1
 
-if [ $NUMBER lt 10 ]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given number is less than 10"
 else
-    echo "guven number is greater than 10"
+    echo "given number is greater than 10"
 fi
