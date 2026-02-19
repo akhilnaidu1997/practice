ef4c4f0d588af497a24baf8e8352a856aed67196
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 21:13:58 +0530

    shell

--- shell/18-functions.sh
+++ shell/18-functions.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USER=$(id -u)
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[0m"
 
 if [ $USER -ne 0 ]; then
     echo "ERROR:: please proceed with sudo previlages"
@@ -9,10 +13,10 @@ fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo "$2 installation is failed"
+        echo -e "$RED $2 installation is failed $N"
         exit 1
     else
-        echo "$2 installation is success"
+        echo -e "$GREEN $2 installation is success $N"
     fi
 }
 
@@ -20,16 +24,22 @@ dnf list installed mysql
 if [ $? -ne 0 ]; then
     dnf install mysql -y 
     VALIDATE $? mysql
+else
+    echo -e "$BLUE mysql is already installed $N"
 fi
 
 dnf list installed nginx
 if [ $? -ne 0 ]; then
     dnf install nginx -y 
     VALIDATE $? nginx
+else
+    echo -e "$BLUE nginx is already installed $N"
 fi
 
 dnf list installed python
 if [ $? -ne 0 ]; then
     dnf install python -y 
     VALIDATE $? python
+else
+    echo -e "$BLUE python is already installed $N"
 fi
