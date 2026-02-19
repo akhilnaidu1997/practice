810d59ab0ac4b0ec44d0f556e4117f07e40951df
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 18:18:50 +0530

    shell

---
+++ shell/11-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# in linux we donot have specific data types
+# linux will consider everything as a string
+
+COURSE="DevSecOps"
+DURATION=120
+
+echo "Course name: ${COURSE} and duration is ${DURATION}"
