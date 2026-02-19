a4a649484c3c1d98eab3f06c23a975052e0d35cb
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-19 18:22:26 +0530

    shell

--- shell/11-data-types.sh
+++ shell/11-data-types.sh
@@ -9,4 +9,7 @@ DURATION=120
 echo "Course name: ${COURSE} and duration is ${DURATION}"
 
 NAME=("MODI" "PUTIN" "TRUMP" "MACRON")
-echo "print name of first in the list :${NAME[@]}"
+echo "print all names in the list :${NAME[@]}"
+echo "print first in list: ${NAME[0]}"
+echo "second in the list: ${NAME[1]}"
+echo "Third in the list: ${NAME[2]}"
