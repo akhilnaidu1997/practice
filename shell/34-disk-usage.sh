8085934b3c904d76591263bc1d8aec3e934807f8
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 17:23:43 +0530

    shell

--- shell/34-disk-usage.sh
+++ shell/34-disk-usage.sh
@@ -6,8 +6,8 @@ MESSAGE=""
 
 while IFS= read -r disk
 do
-    USAGE=$( $disk | | awk '{print $5}' | cut -d "%" -f1)
-    PARTITION=$(df -h | grep -v Filesystem | awk '{print $6}')
+    USAGE=$(($disk | | awk '{print $5}' | cut -d "%" -f1))
+    PARTITION=$((df -h | grep -v Filesystem | awk '{print $6}'))
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
         MESSAGE+="High disk usage on $PARTITION: $DISK_USAGE %"
     fi
