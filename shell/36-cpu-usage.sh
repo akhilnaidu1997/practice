3289464523d6a3b4266ca7366fa8bdc2f9ce71a1
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 18:21:04 +0530

    shell

--- shell/36-cpu-usage.sh
+++ shell/36-cpu-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+CPU_IDLE=$(mpstat 1 1 | awk '/Average/ {print $NF}')
 CPU_USAGE=$(awk "BEGIN {print 100 - $CPU_IDLE}")
 printf "CPU Usage: %.2f%%\n" "$CPU_USAGE"
