c80128fbf6fd523e5c5942e064ec1bfd51e32a98
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 10:46:34 +0530

    shell

--- shell/30-while.sh
+++ shell/30-while.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-count=5
-while [ $count -gt 0 ]
+# count=5
+# while [ $count -gt 0 ]
+# do
+#     echo "count $count"
+#     count=$((count-1))
+# done
+
+while IFS= read -r line 
 do
-    echo "count $count"
-    count=$((count-1))
-done
+    echo "Line: $line"
+done < 29-catalogue.sh
