2ef44184af077d5eaab0a53c96065c52ab5b8573
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 10:18:45 +0530

    shell

--- shell/29-catalogue.sh
+++ shell/29-catalogue.sh
@@ -49,7 +49,7 @@ VALIDATE $? "/app"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
-unzip /tmp/catalogue.zip
+unzip -o /tmp/catalogue.zip
 VALIDATE $? "unzip"
 
 npm install
@@ -63,7 +63,7 @@ systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y
 
