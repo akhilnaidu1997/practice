2c51839c5436ee10538518ed324d6fd045615ff0
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 17:27:44 +0530

    shell

--- shell/34-disk-usage.sh
+++ shell/34-disk-usage.sh
@@ -9,7 +9,7 @@ do
     USAGE=$(echo $disk | awk '{print $5}' | cut -d "%" -f1)
     PARTITION=$(echo $disk | awk '{print $6}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High disk usage on $PARTITION: $DISK_USAGE %"
+        MESSAGE+="High disk usage on $PARTITION: $USAGE %"
     fi
 done <<< $DISK_USAGE
 
