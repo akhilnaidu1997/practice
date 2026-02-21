122b25b077eaa6a25eb125e0562505203923fe07
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 17:49:18 +0530

    shell

--- shell/35-ram-usage.sh
+++ shell/35-ram-usage.sh
@@ -2,7 +2,10 @@
 
 RAM_USAGE=$(free -m | awk '/Mem:/ {printf("RAM Used: %.2f%%\n", $3/$2*100)}')
 RAM_THRESHOLD=20
+MESSAGE=""
 
 if [ $RAM_USAGE -ge $RAM_THRESHOLD ]; then
     MESSAGE="High RAM usage: $RAM_USAGE"
 fi
+
+echo "Message body: $MESSAGE"
