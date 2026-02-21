37c8bf861518ec5f3285c4ebfcfc70d18bb8ac07
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-21 18:27:10 +0530

    shell

--- shell/36-cpu-usage.sh
+++ shell/36-cpu-usage.sh
@@ -3,3 +3,14 @@
 IDLE=$(top -b -n2 | grep "Cpu(s)" | tail -1 | awk '{print $8}')
 CPU_USAGE=$(echo "scale=2; 100 - $IDLE" | bc)
 echo "CPU Usage: $CPU_USAGE%"
+
+
+# top command
+# IDLE cpu % and we cant calculate 100-$IDLE to get the used %
+# echo "cpu usage: $USED"
+
+#RAM from free -h
+# get the used RAM used awk cmd
+# calculate used RAM/total RAM * 100
+# print the RAM percentage used and send mail if goes beyond htreshodl
+
