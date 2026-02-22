6a7b3499fff8655ecb703c4e93dbd4c8db072c11
Akhil Naidu <akhilnaid1997@gmail.com>
2026-02-22 18:15:55 +0530

    shell

--- shell/36-cpu-usage.sh
+++ shell/36-cpu-usage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Capture the second iteration of top for accurate idle time
 IDLE=$(top -b -n2 | grep "Cpu(s)" | tail -1 | awk '{print $8}')
-CPU_USAGE=$(echo "scale=2; 100 - $IDLE" | bc)
+CPU_USAGE=$(echo "scale=2; 100 - $IDLE" | bc) # bc (basic calculator)
 echo "CPU Usage: $CPU_USAGE%"
 
+#simple
+top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"% used"}'
 
 # top command
 # IDLE cpu % and we cant calculate 100-$IDLE to get the used %
--- shell/examples.sh
+++ shell/examples.sh
@@ -10,3 +10,40 @@ str="DevOpsEngineer"
 echo ${#str}
 echo ${str:0:6}
 echo ${str/Engineer/Admin}
+---
+
+count=5
+
+while [ $count -le 5]
+do
+    echo "count: $count"
+    ((count--))
+done
+
+---
+#!/bin/bash
+
+file=$1
+
+if [ -f $file ]; then
+    echo "file exists"
+else
+    echo "file $file doesnot exists"
+fi
+
+---
+#!/bin/bash
+
+DIR=$1
+
+if [ -d $DIR ]; then
+    echo "directory $DIR exists"
+else
+    echo "directory $DIR doesnot exists.. creating"
+    mkdir -p $DIR
+fi
+
+# Show top 5 CPU process
+ps -eo pid,ppid,comm,%cpu --sort=-%cpu | head -n 6
+
+IDLE=$(top -bn1 | grep Cpu(s) | awk '{print $5})
